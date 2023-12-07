ff0b5843488b7e612738f49db08655ae04af527b
DivyaJ4github <103642896+DivyaJ4github@users.noreply.github.com>
2023-12-07 21:41:09 +0530

    Update divya_demo_app.py

--- divya_demo_app.py
+++ divya_demo_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("Divya's tds demo app 1")
+st.header("Subtraction between 2 numbers")
+def subtraction(a,b):
+  return a-b
+
+a = st.number_input("Enter first number:")
+b =st.number_input("Enter second number:")
+st.write("First Number is : ",a)
+st.write("Second Number is : ",b)
+st.write("Difference:", subtraction(a,b))
